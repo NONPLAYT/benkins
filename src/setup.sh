08fb9f9bc03d81383ce94d8b2227e408922cd304
NONPLAYT <kidzania177@gmail.com>
2023-01-13 01:36:16 +0300

    trying to fix

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,4 @@
-git config user.name "BX-Team-Bot"
+git config user.name ${USERNAME}
 git config user.email ${LOGIN_EMAIL}
 
 git remote set-url origin https://${ACCESS_TOKEN}@github.com/NONPLAYT/benkins.git
